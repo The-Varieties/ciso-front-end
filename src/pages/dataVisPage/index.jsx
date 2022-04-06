6831fa83623c1bcdca0327f9fb1917cf4a31c710
tensan8 <tensanf@gmail.com>
2022-04-06 14:54:14 +0700

    Added dummy visualization SVGs

--- src/pages/dataVisPage/index.jsx
+++ src/pages/dataVisPage/index.jsx
@@ -9,6 +9,9 @@
 
 import Card from "../../components/cards";
 
+import pie_chart from "../../assets/DummyImages/pie_chart.svg";
+import line_graph from "../../assets/DummyImages/line_graph.svg";
+
 function DataVisPage() {
     const content = (
         <div className="pr-96 pb-80">
@@ -20,11 +23,15 @@ function DataVisPage() {
     return (
         <div>
             <h1 className="w-fit">Top Component</h1>
-            <h1 className="mt-10 w-fit">Graphs</h1>
+
+            <div className="flex mt-10">
+                <img src={pie_chart} alt="Dummy Pie Chart" className="w-5/12" />
+                <img src={line_graph} alt="Dummy Line Graph" className="w-7/12 ml-4"/>
+            </div>
 
             {/* Layout data vis page */}
 
-            <div className="mt-10 mb-10">
+            <div className="mt-16 mb-10">
                 <Card cardContent = {content} />
             </div>
         </div>
