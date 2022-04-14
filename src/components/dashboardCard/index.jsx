426dc2a0e5f6cb6737246e6e170304f61e83a2c5
jentz2909 <65669574+jentz2909@users.noreply.github.com>
2022-04-14 23:34:22 +0800

    Trying on delete icon in delete instance

---
+++ src/components/dashboardCard/index.jsx
@@ -0,0 +1,46 @@
+import RightArrow from "../../assets/Icons/right_arrow.svg";
+import { useNavigate } from 'react-router-dom';
+
+function DashboardCard(props) {
+    let navigate = useNavigate();
+
+    const goNextPage = () => {
+        if (props.hasOnClick)
+            navigate(props.nextPageRoute);
+    }
+
+    const deleteicon = () => {
+        if (props.clickdelete)
+            navigate(props.clickdeleteinstance);
+    }
+
+    return (
+        <div className={
+            `py-5 px-10 w-full h-fit bg-white rounded-3xl shadow-lg shadow-black/50 bg-gradient-to-t 
+            ${props.cardContent.instanceStatus === "Optimized" ? "from-card-green to-card-green/20" : (props.cardContent.instanceStatus === "Underutilized" ? "from-card-yellow to-card-yellow/20" : "from-card-red to-card-red/20")}
+        `}>
+            <div className="relative pb-10">
+                <p>ID: {props.cardContent.id}</p>
+                <h1 className="font-extrabold text-3xl pt-3 pb-1">{props.cardContent.name}</h1>
+                <p><span className="font-bold">Status: </span>{props.cardContent.instanceStatus}</p>
+                <p><span className="font-bold">IP Address: </span>{props.cardContent.ipAddress}</p>
+
+                <div className="absolute right-0 top-0">
+                    <div class="deletebutton" onClick={deleteicon}>
+                        <button>Delete instance</button>
+                    </div>
+                </div>
+
+                <div className="absolute right-0 bottom-0">
+                    <div className="flex cursor-pointer" onClick={goNextPage}>
+                        <p className="italic">See More</p>
+                        <img src={RightArrow} alt="Next Arrow"/>
+                    </div>
+                </div>
+                
+            </div>    
+        </div>
+    )
+}
+
+export default DashboardCard;
