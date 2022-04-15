e788e5b1298c56f5af078cfeab1f23e9e63504fc
jentz2909 <65669574+jentz2909@users.noreply.github.com>
2022-04-15 20:42:48 +0800

    Update index.jsx

--- src/components/backArrow/index.jsx
+++ src/components/backArrow/index.jsx
@@ -1,2 +1,17 @@
 // This component will draw the back arrow and take a route as a parameter
 // onClick, this component will direct the users to the passed route from the parameter
+
+import backArrow from "../../assets/Icons/back_arrow.svg";
+import { useNavigate } from 'react-router-dom';
+
+function BackArrow(props) {
+    let navigate = useNavigate();
+
+    return(
+        <div className="my-auto">
+            <img src={backArrow} alt="Back Arrow" className="h-7 cursor-pointer" onClick={(e) => {navigate("/")}}/>
+        </div>
+    )
+}
+
+export default BackArrow;
