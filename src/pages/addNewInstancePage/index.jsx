3efc075b4d7cc90b868114603d1260ecc1424497
jentz2909 <65669574+jentz2909@users.noreply.github.com>
2022-04-15 23:38:28 +0800

    Update index.jsx

--- src/pages/addNewInstancePage/index.jsx
+++ src/pages/addNewInstancePage/index.jsx
@@ -1,3 +1,25 @@
 // Components:
 // - formInputField
 // - customFormButton
+
+import React from 'react';
+
+function AddInstance(props){
+
+    const d = () => {
+        if (props.hasOnClick)
+            navigate(props.nextPageRoute);
+    }
+
+    return (
+        <div>
+            <p>asd</p>
+        </div>>
+        :
+        <div>
+        <p>asd</p>
+        </div>
+    )
+}
+
+export default AddInstance;
