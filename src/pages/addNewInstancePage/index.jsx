393e9ad23bc5612ebdfa4d9d4c161191d3c0bfb6
jentz2909 <65669574+jentz2909@users.noreply.github.com>
2022-04-16 10:52:25 +0800

    update addinstance

--- src/pages/addNewInstancePage/index.jsx
+++ src/pages/addNewInstancePage/index.jsx
@@ -14,15 +14,15 @@ function AddInstance(props){
         <form class="form"action="#">
         <h1>Adding Instance</h1>
             <div class="inputContainer">
-                <label for="" class="label">Instance Name: </label>
+                <label for="" class="label">Instance Name: </label><br/>
                 <input type="text" class="input"></input>
             </div>
             <div class="inputContainer">
-                <label for="" class="label">IP Address: </label>
+                <label for="" class="label">IP Address: </label><br/>
                 <input type="text" class="input"></input>
             </div>
 
-            <button type="submit" class="submitBtn" value="Add"></button >
+            <button  type="submit" class="addbtn" value="Add">Add Instance</button >
         </form>
         </div>
     )
