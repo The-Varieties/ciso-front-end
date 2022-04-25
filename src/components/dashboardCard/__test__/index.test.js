2cc8dd28165cbc9d2ab75b886f61d70bebf8ad95
tensan8 <tensanf@gmail.com>
2022-04-25 19:58:59 +0700

    need fixes

--- src/components/dashboardCard/__test__/index.test.js
+++ src/components/dashboardCard/__test__/index.test.js
@@ -1,25 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import DashboardCard from '..';
-import { shallow } from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
 import Enzyme from 'enzyme';
 import store from '../../../store/store';
+import DashboardCard from '..';
+import configureStore from 'redux-mock-store';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import renderer from 'react-test-renderer';
 
+
+const mockStore = configureStore([]);
 
 it("Renders without crashing", () => {
     const root = ReactDOM.createRoot(document.createElement("div"));
     root.render(<DashboardCard></DashboardCard>);
 })
 
-it("Render Card correctly", () => {
+describe("Test", () => {
     Enzyme.configure({ adapter: new Adapter() });
-
+    let store;
+    let component;
     let instance = {name: "Instance 1", id: "de8q9dn9", ipAddress: "192.168.77.43", instanceStatus: "Optimized"};
 
-    <Provider store={store}>
-        const dashCard = shallow(<DashboardCard cardContent = {instance} hasOnClick = {true} nextPageRoute = {"/data-vis-page"} />);
-        expect(dashCard.text()).toContain("Instance 1");
-    </Provider>
+    beforeEach(() => {
+        store = mockStore({
+            instance: [],
+            loading: true,
+            instanceList: [],
+        });
+
+        component = renderer.create(
+            <BrowserRouter>
+                <Provider store={store}>
+                    <DashboardCard cardContent = {instance} hasOnClick = {true} nextPageRoute = {"/data-vis-page"}/>
+                </Provider>
+            </BrowserRouter>
+        )
+    })
+
+    it("Render Card correctly", () => {
+        expect(component.toJSON().children[0].children[0].children[1]).toEqual('de8q9dn9');
+
+        // console.log(component.root.findByType('button').props.onClick())
+    })
 })
