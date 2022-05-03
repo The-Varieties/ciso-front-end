5526358cbd5d50d48a99b74bc9a591a801835c42
jentz2909 <65669574+jentz2909@users.noreply.github.com>
2022-05-03 14:11:00 +0800

    register and login page

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,14 +1,35 @@
 import React from "react";
-
+import './index.css';
+import { Link } from 'react-router-dom';
 
 function LoginModule(){
 
 
     return(
-        <div class="">
-            <div>
-                
+        <div className="justify-content-center">
+            <h1 class="title">
+                Sign in to your account
+            </h1>
+            <p class="sentenses">
+                Start using our product
+            </p>
+            
+            <div className="logintable">
+            <form>
+                <div className="input-container">
+                <label>User Name </label>
+                <input type="text" name="uname" required size="50"/>
+                </div>
+                <div className="input-container">
+                <label>Password </label>
+                <input type="password" name="pass" required size="50"/>
+                </div>
+                <div className="button-container">
+                    <input type="submit" value="Sign In"/>
+                </div>
+            </form>
             </div>
+            <p class="registerlink">Don’t have an account? <a><Link to={"/register-page"}>Register</Link></a></p>
         </div>
     )
 }
--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -1,14 +1,53 @@
 import React from "react";
+import './index.css';
+import { Link } from 'react-router-dom';
 
-
-function RegisterModule(){
+function LoginModule(){
 
 
     return(
-        <div>
+        <div className="justify-content-center">
+            <h1 class="title">
+                Register
+            </h1>
+            <p class="sentenses">
+                Start using our product
+            </p>
 
+            <div className="registertable">
+            <form>
+                <div className="input-container">
+                <label>User Name </label>
+                <input type="text" name="uname" required size="50"/>
+                </div>
+                <div className="input-container">
+                <label>First Name </label>
+                <input type="text" name="uname" required size="50"/>
+                </div>
+                <div className="input-container">
+                <label>Last Name </label>
+                <input type="text" name="uname" required size="50"/>
+                </div>
+                <div className="input-container">
+                <label>Email </label>
+                <input type="text" name="uname" required size="50"/>
+                </div>
+                <div className="input-container">
+                <label>Password </label>
+                <input type="password" name="pass" required size="50"/>
+                </div>
+                <div className="input-container">
+                <label>Confirm Password </label>
+                <input type="password" name="pass" required size="50"/>
+                </div>
+                <div className="button-container">
+                    <input type="submit" value="Register"/>
+                </div>
+            </form>
+            </div>
+            <p class="registerlink">Already have an account?<a><Link to={"/login-page"}> Sign In</Link></a></p>
         </div>
     )
 }
 
-export default RegisterModule;
+export default LoginModule;
