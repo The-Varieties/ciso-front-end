63ba59ab8fe010f31dcef22bb3ef2950c33ef79d
jentz2909 <65669574+jentz2909@users.noreply.github.com>
2022-05-07 19:34:52 +0800

    fixing adding instance page

--- src/pages/addNewInstancePage/index.jsx
+++ src/pages/addNewInstancePage/index.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 // import ReactDOM from 'react-dom';
-import './style.css';
+import './index.css';
 import { useNavigate } from 'react-router-dom';
 
 function AddInstance(){
