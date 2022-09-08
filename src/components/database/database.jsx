9fff466dbdf5b1cadc5eb989adc8385eb120d587
jentz2909 <65669574+jentz2909@users.noreply.github.com>
2022-09-08 23:50:11 +0800

    insert database table

--- src/components/database/database.jsx
+++ src/components/database/database.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import database from '../../assets/Images/database.png';
+import './index.css';
 import BackArrow from "../../components/backArrow";
 
 
@@ -13,9 +13,46 @@ function DatabasePage(){
             <BackArrow backPath = "/"/>
             </div>
 
-            <div className="mx-40">
-                <img src={database}></img>
-            </div>
+            <table>
+            <tr>
+                <th>Name</th>
+                <th>RAM</th>
+                <th>Storage</th>
+                <th>CPU</th>
+                <th>IPv4</th>
+            </tr>
+            <tr>
+                <td>Instance 1</td>
+                <td>4 GB</td>
+                <td>12 GB</td>
+                <td>t2.xlarge</td>
+                <td>192.168.0.0</td>
+            </tr>
+            <tr>
+                
+            </tr>
+            <tr>
+                <td>Instance 2</td>
+                <td>1 GB</td>
+                <td>2 GB</td>
+                <td>t2.micro</td>
+                <td>164.172.66.55</td>
+            </tr>
+            <tr>
+                <td>Instance 3</td>
+                <td>2 GB</td>
+                <td>6 GB</td>
+                <td>t2.small</td>
+                <td>77.156.66.40</td>
+            </tr>
+            <tr>
+                <td>Instance 4</td>
+                <td>1 GB</td>
+                <td>4 GB</td>
+                <td>t2.medium</td>
+                <td>22.22.65.141</td>
+            </tr>
+            </table>
         </div>
     )
 }
