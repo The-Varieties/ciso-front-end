cee1425494746084d747072177416ace8c16d45d
jentz2909 <65669574+jentz2909@users.noreply.github.com>
2022-09-09 12:04:33 +0800

    latest version of database table

--- src/components/database/database.jsx
+++ src/components/database/database.jsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { useState }  from "react";
 import './index.css';
 import BackArrow from "../../components/backArrow";
+import data from "./testing-data.json";
 
 
-function DatabasePage(){
+const DatabasePage=()=>{
 
+    const [passdatabases, setPassDatabases] = useState(data);
 
     return(
-        <div class="">
-            
+        <div>
             <div class="backarrow">
             <BackArrow backPath = "/"/>
             </div>
 
-            <table>
+            <div class="database-container">
+            <table class="db-table">
+            <thead>
             <tr>
                 <th>Name</th>
                 <th>RAM</th>
@@ -21,40 +24,22 @@ function DatabasePage(){
                 <th>CPU</th>
                 <th>IPv4</th>
             </tr>
-            <tr>
-                <td>Instance 1</td>
-                <td>4 GB</td>
-                <td>12 GB</td>
-                <td>t2.xlarge</td>
-                <td>192.168.0.0</td>
+            </thead>
+            <tbody>
+                {passdatabases.map((passdatabase)=> (
+                <tr>
+                <td>{passdatabase.Name}</td>
+                <td>{passdatabase.RAM}</td>
+                <td>{passdatabase.Storage}</td>
+                <td>{passdatabase.CPU}</td>
+                <td>{passdatabase.IPv4}</td>
             </tr>
-            <tr>
-                
-            </tr>
-            <tr>
-                <td>Instance 2</td>
-                <td>1 GB</td>
-                <td>2 GB</td>
-                <td>t2.micro</td>
-                <td>164.172.66.55</td>
-            </tr>
-            <tr>
-                <td>Instance 3</td>
-                <td>2 GB</td>
-                <td>6 GB</td>
-                <td>t2.small</td>
-                <td>77.156.66.40</td>
-            </tr>
-            <tr>
-                <td>Instance 4</td>
-                <td>1 GB</td>
-                <td>4 GB</td>
-                <td>t2.medium</td>
-                <td>22.22.65.141</td>
-            </tr>
+            ))}
+            </tbody>
             </table>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default DatabasePage;
