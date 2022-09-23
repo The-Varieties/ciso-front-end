0de71e9d9a3fc765c15e61e9deda2f0ab43d58dc
jentz2909 <65669574+jentz2909@users.noreply.github.com>
2022-09-23 12:32:36 +0800

    Login module error message

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,18 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import './index.css';
-import { Link, useNavigate} from 'react-router-dom';
+import { Link, useNavigate,ReactDOM} from 'react-router-dom';
 import logo from '../../assets/Images/logo.png';
 
 function LoginModule(){
 
+    /// Error Message
+    const [errorMessages, setErrorMessages] = useState({});
+
+    /// Success log in
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    // User info
+    const database = [
+        {
+          username: "bryan",
+          password: "123"
+        }
+    ];
+
+    // Error message
+    const errors = {
+        uname: "Username or Password invalid",
+        pass: "Username or Password invalid"
+    };
+
     let navigate = useNavigate();
 
-    const onSubmitHandler = (e) => {
-        e.preventDefault();
+    const handleSubmit = (event) => {
+        //Prevent page reload
+        event.preventDefault();
         navigate("/");
-    }
+    
+        var { uname, pass } = document.forms[0];
+    
+        // Find user login info
+        const userData = database.find((user) => user.username === uname.value);
+    
+        // Compare user info
+        if (userData) {
+          if (userData.password !== pass.value) {
+            // Invalid password
+            setErrorMessages({ name: "pass", message: errors.pass });
+          } else {
+            setIsSubmitted(true);
+          }
+        } else {
+          // Username not found
+          setErrorMessages({ name: "pass", message: errors.uname });
+        }
+    };
 
-    return(
+    const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+
+    const renderForm =(
         <div className="justify-content-center">
             <div class="icon">
                 <img src={logo}></img>
@@ -25,14 +70,16 @@ function LoginModule(){
             </p>
             
             <div className="logintable">
-            <form onSubmit={onSubmitHandler}>
+            <form onSubmit={handleSubmit}>
                 <div className="input-container">
                 <label>User Name </label>
                 <input type="text" name="uname" required size="50"/>
+                {renderErrorMessage("uname")}
                 </div>
                 <div className="input-container">
                 <label>Password </label>
                 <input type="password" name="pass" required size="50"/>
+                {renderErrorMessage("pass")}
                 </div>
                 <div className="button-container">
                     <input type="submit" value="Sign In"/>
@@ -41,7 +88,13 @@ function LoginModule(){
             </div>
             <p class="registerlink">Don’t have an account? <a><Link to={"/register-page"}>Register</Link></a></p>
         </div>
-    )
+    );
+
+    return (
+    <div>
+        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+    </div>
+    );
 }
 
 export default LoginModule;
