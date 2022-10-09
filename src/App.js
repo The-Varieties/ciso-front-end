33142e05d86d10bec1c29dccea6078dcf486a4e3
jentz2909 <65669574+jentz2909@users.noreply.github.com>
2022-10-09 22:59:12 +0800

    redux testing

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FinancialReport from "./components/financialReport";
-import ProfilePage from "./components/profile";
-import AddInstance from "./pages/addNewInstancePage";
-import Dashboard from './pages/dashboard';
-import DataVisPage from './pages/dataVisPage';
-import DatabasePage from "./components/database/database";
-import LoginModule from "./components/login/login";
-import RegisterModule from "./components/register/register";
+import React from 'react';
+import Main from './components/zzzmytestingfile/Main';
+import Navbar from './components//zzzmytestingfile/Navbar';
+import { Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path = '/' element={<Dashboard />} />
-          <Route path = '/dashboard' element={<Dashboard />} />
-          <Route path = '/data-vis-page' element={<DataVisPage />} />
-          <Route path = '/add-new-instance' element={<AddInstance />} />
-          <Route path = '/profile' element={<ProfilePage />}/>
-          <Route path = '/financial-report' element={<FinancialReport />}/>
-          <Route path = '/database-page' element={<DatabasePage />}/>
-          <Route path = '/login-page' element={<LoginModule />}/>
-          <Route path = '/register-page' element={<RegisterModule />}/>
-        </Routes>
-      </BrowserRouter>
+const App = () => (
+  <div>
+    <Navbar />
+    <div className="container">
+      <Main />
     </div>
-  );
-}
+    <div className="fixed-action-btn">
+      <Link to="/meetups/add" className="btn-floating btn-large red">
+        <i className="fa fa-plus"></i>
+      </Link>
+    </div>
+  </div>
+)
 
 export default App;
