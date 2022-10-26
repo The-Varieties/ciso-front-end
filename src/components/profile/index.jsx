1fda2dd4cced2021a0267c24890fa92f8ff3bf8c
Alesandro Michael Ferdinand <tensanf@gmail.com>
2022-10-27 00:44:45 +0800

    Fixed integration of user profile

--- src/components/profile/index.jsx
+++ src/components/profile/index.jsx
@@ -1,43 +1,24 @@
-import React,{useState} from "react";
+import React, {useEffect, useState} from "react";
 import './index.css';
 import profile from '../../assets/Images/profile.png'
 import BackArrow from "../backArrow";
 import {useLocation} from 'react-router-dom';
 import { getUser } from "../../store/actions/userAction";
 import { connect } from "react-redux";
-import { GetUserIdFromToken } from "../../utils/tokenDecoder";
+// import { GetUserIdFromToken } from "../../utils/tokenDecoder";
 
 
 function ProfilePage(props) {
-
     const location = useLocation()
     const { from } = location.state
+    const [profiledata, setProfileData] = useState(null)
 
-    props.getUser(GetUserIdFromToken());
-    const profiledata = useState(props.userData);
-    // console.log(GetUserIdFromToken()) // can use this function to get the id
-
-    //const userid1 = props.getUser(GetUserIdFromToken());
-
-    // const getinfo = props.userData;
-    // props.userData
-
-    //console.log(props.userData);
-    //console.log(profiledata);
-
-    // const data1 = props.getUser(GetUserIdFromToken());
-    
-    // console.log(data1);
-
-    // useEffect((profiledata) => {
-    //     if(profiledata == null){
-    //         props.getUser(GetUserIdFromToken());
-    //     } else if(props.userData !== null && props.userData.length !== 0) {
-    //         props.getUser(GetUserIdFromToken());
-    //     } else{
-    //         GetUserIdFromToken();
-    //     }
-    // }, [props.userData])
+    useEffect(() => {
+        if(profiledata === null || profiledata.length === 0) {
+            props.getUser(1);
+            setProfileData(props.userData);
+        }
+    },[props.userData])
 
     return (
     <div className="m-0 p-0">
@@ -51,28 +32,32 @@ function ProfilePage(props) {
             <hr class="border-2 border-black bg-black"/>
             <div className="mt-4 grid grid-cols-2 gap-4">
                 <div className="">
-                    <h2 className="text-emerald-400 font-semibold text-[24px]">First Name</h2>
-                    {profiledata.map((profiledata, index)=> (
-                    <p key={index} className="text-emerald-400 text-[18px]">{profiledata.user_firstname}</p>
-                    ))}
+                    <h2 className="text-emerald-400 font-semibold text-[20px]">First Name</h2>
+                    {profiledata != null
+                        ? <p className="text-emerald-400 text-[18px]">{profiledata.user_firstname}</p>
+                        : <div></div>
+                    }
                 </div>
                 <div className="">
                     <h2 className="text-emerald-400 font-semibold text-[20px]">Last Name</h2>
-                    {profiledata.map((profiledata, index)=> (
-                    <p key={index} className="text-emerald-400 text-[18px]">{profiledata.user_lastname}</p>
-                    ))}
+                    {profiledata != null
+                        ? <p className="text-emerald-400 text-[18px]">{profiledata.user_lastname}</p>
+                        : <div></div>
+                    }
                 </div>
                 <div className="">
                     <h2 className="text-emerald-400 font-semibold text-[20px]">Username</h2>
-                    {profiledata.map((profiledata, index)=> (
-                    <p key={index} className="text-emerald-400 text-[18px]">{profiledata.user_username}</p>
-                    ))}
+                    {profiledata != null
+                        ? <p className="text-emerald-400 text-[18px]">{profiledata.user_username}</p>
+                        : <div></div>
+                    }
                 </div>
                 <div className="">
                     <h2 className="text-emerald-400 font-semibold text-[20px]">Email</h2>
-                    {profiledata.map((profiledata, index)=> (
-                    <p key={index} className="text-emerald-400 text-[18px]">{profiledata.user_email}</p>
-                    ))}
+                    {profiledata != null
+                        ? <p className="text-emerald-400 text-[18px]">{profiledata.user_email}</p>
+                        : <div></div>
+                    }
                 </div>
             </div>
         </div>
